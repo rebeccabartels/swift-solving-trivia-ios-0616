f4ff2d6f0bd967dc09fc8c289d694e2ab140dfbf
rebeccabartels <rebeccakbartels@icloud.com>
2016-07-18 14:00:43 -0400

    done

--- swift-solving-trivia/AppDelegate.swift
+++ swift-solving-trivia/AppDelegate.swift
@@ -12,11 +12,26 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
-    /*
-     
-     write your function here
-     
-     */
-
+    func solveTrivia(statesAndCapitals : [String: String]) -> String {
+        
+        var isMatch = false
+        
+        for (state, capital) in statesAndCapitals {
+            
+            let stateSet = Set(state.lowercaseString.characters).sort()   //what is set doing here? has to be case sens. 
+            let capitalSet = Set(capital.lowercaseString.characters).sort()
+            isMatch = (stateSet.map { capitalSet.contains($0) }).contains(true)
+            
+            if !isMatch {
+                
+                return state
+            }
+            
+        }
+        
+        return "No answer found"
+        
+    }
+    
+    
 }
-
